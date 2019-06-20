40e47ec162084d0c5c631aa2976bacdcf3f9f813
sharonmaswai <chepmaswai@gmail.com>
2019-06-20 14:43:42 +0300

    create db and create image model

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/')
+    image_name = models.CharField(max_length=30)
+    image_caption = HTMLField()
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    likes = models.CharField(max_length =50)
+    comments = models.CharField(max_length =50)
+
