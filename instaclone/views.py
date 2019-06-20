59f32d8f3b700026d16c81fa4697f5f4f0c51d17
sharonmaswai <chepmaswai@gmail.com>
2019-06-20 16:50:26 +0300

    add login view

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required.
+
 
 def index(request):
 
     return render(request, 'display/index.html')
-
+@login_required(login_url='/accounts/login/')
+def article(request, article_id):
