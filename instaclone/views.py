799c1d822b7b9c58b06d2fc74f94d7c54facc32d
sharonmaswai <chepmaswai@gmail.com>
2019-06-21 10:42:38 +0300

    create view for new profile and display profile page

--- instaclone/views.py
+++ instaclone/views.py
@@ -6,3 +6,22 @@ def index(request):
 
     return render(request, 'display/index.html')
 
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+            profile.user_id=current_user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'create-profile.html', {"form":form})
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+    posts=Image.objects.filter(profile_id=current_user.id)
+    return render(request, 'profile.html',{"profile":profile,"posts":posts})
