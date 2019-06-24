4254c16ca96828fb68822d4b7db877f05ee960f4
sharonmaswai <chepmaswai@gmail.com>
2019-06-24 11:14:44 +0300

    create post image form

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -5,3 +5,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user_id']
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        exclude = ['likes', 'comment', 'image_name']
+       
